54e602e610fff754652f8f0232c5c12b48b40e34
TD-11 <tulio.s@escolar.ifrn.edu.br>
2025-02-28 14:28:00 -0300

    Faltam acabamentos

--- trabalho de MADJ/Assets/Scripts/Player.cs
+++ trabalho de MADJ/Assets/Scripts/Player.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class Scripts : MonoBehaviour
 {
    private Rigidbody2D rig;
+   private Animator anim;
 
-   public float velocity;
+   private float velocity = 3f;
    public float forceJump;
    private bool jumping;
+   private bool attacking;
+   private bool grounded;
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -33,10 +37,16 @@ public class Scripts : MonoBehaviour
             if (Input.GetAxis("Horizontal") > 0f)
             {
                 transform.eulerAngles = new Vector3(0f, 0f, 0f);
+                anim.SetBool("Walking", true);
             }
             if (Input.GetAxis("Horizontal") < 0f)
             {
                 transform.eulerAngles = new Vector3(0f, 180f, 0f);
+                anim.SetBool("Walking", true);
+            }
+            if (Input.GetAxis("Horizontal") == 0f)
+            {
+                anim.SetBool("Walking", false);
             }
         }
 
@@ -47,6 +57,7 @@ public class Scripts : MonoBehaviour
             if (!jumping)
             {
                 rig.velocity = Vector2.up * forceJump;
+                anim.SetBool("Jumping", true);
             }
          
         } 
@@ -56,7 +67,14 @@ public class Scripts : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.L))
         {
-            Debug.Log("Ataque");
+            if (!attacking)
+            {
+                anim.SetBool("Attacking", true);
+            }
+            if(!attacking && grounded == true)
+            {
+                velocity = 0f;
+            }
         }
     }
 
@@ -64,11 +82,9 @@ public class Scripts : MonoBehaviour
     {
         if (collision.gameObject.tag == "Ground")
         {
+            anim.SetBool("Jumping", false);
             jumping = false;
-        }
-        if (collision.gameObject.tag == "Danger")
-        {
-            Destroy(gameObject);
+            grounded = true;
         }
     }
     private void OnCollisionExit2D(Collision2D collision)
@@ -76,6 +92,14 @@ public class Scripts : MonoBehaviour
         if (collision.gameObject.tag == "Ground")
         {
             jumping = true;
+            grounded = false;
         }
+    }
+
+    void EndingAnimation()
+    {
+        anim.SetBool("Attacking", false);
+        velocity = 5f;
+        attacking = false;
     }
 }
